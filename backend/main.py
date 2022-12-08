b5a02b57c784053a03120ff607e3f2b09ffb0e08
David Power <david.lpower@gmail.com>
2022-12-08 12:45:14 +0100

    Refactor lookup code for reuse between endpoints

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from db_manager import DBManager
 from related_artists import RelatedArtists
 from gig_list_parser import GigListParser
 from collections import defaultdict
+import json
 
 server = Flask(__name__)
 
@@ -21,43 +22,7 @@ giglist = None
 if not giglist:
     giglist = gigListParser.get_gig_list()
 
-
-@server.route('/')
-def displayGigs():
-    global conn
-    fav_artists = conn.get_all_favourite_artists()
-
-    response = ""
-    if giglist:
-        for artist in fav_artists:
-            # find all related artists
-            related_artists = relatedArtists.get_related_artists(artist)[0:40]
-            response += f"<h2>Because you like {artist}</h2>"
-            response += "<ul>"
-
-            match_found = False
-            if related_artists:
-                for possible_match in related_artists:
-                    for gig in giglist:
-                        if possible_match in gig['artist']:
-                            match_found = True
-                            status = 'still on' if gig['status'] == '' else gig['status']
-                            response += f"<li><b>{gig['artist']}</b> is playing in <b>{gig['venue']}</b> on the <b>{gig['date']}</b> - Status: <b>{status}</b> <code title='Recommendation based off artist {possible_match}'>&#9432;</code></li>"
-                if not match_found:
-                    response += f'<li><i>There are no gigs in Berlin by artists similar to {artist}.</i></li></ul></br>'
-                else:
-                    response += '</ul></br>'
-            else:
-                response += f'<li><i>There are no gigs in Berlin by artists similar to {artist}.</i></li></ul></br>'
-
-    else:
-        response = '<h1>No Gigs Found In Berlin!</h1>'
-
-    return response
-
-
-@server.route('/artists')
-def artists():
+def createNewGigList():
     global conn
     fav_artists = conn.get_all_favourite_artists()
 
@@ -73,12 +38,40 @@ def artists():
                     for gig in giglist:
                         if possible_match in gig['artist']:
                             recommendation = {
-                                "Artist": gig['artist'],
-                                "Venue": gig['venue'],
-                                "Date": gig['date'],
-                                "Status": 'still on' if gig['status'] == '' else gig['status']
+                                "artist": gig['artist'],
+                                "venue": gig['venue'],
+                                "date": gig['date'],
+                                "status": 'still on' if gig['status'] == '' else gig['status'],
+                                "source": possible_match
                             }
                             response[artist].append(recommendation)
+    return response
+
+@server.route('/')
+def displayGigs():
+    artists_and_gigs = createNewGigList()
+    response = "" 
+
+    if artists_and_gigs:
+        for artist, gigs in artists_and_gigs.items():
+            response += f"<h2>Because you like {artist}</h2>"
+            response += "<ul>"
+            if gigs:
+                for gig in gigs:
+                    status = 'still on' if gig['status'] == '' else gig['status']
+                    response += f"<li><b>{gig['artist']}</b> is playing in <b>{gig['venue']}</b> on the <b>{gig['date']}</b> - Status: <b>{status}</b> <code title='Recommendation based off artist {gig['source']}'>&#9432;</code></li>"
+                response += "</ul></br>"
+            else:
+                response += f'<li><i>There are no gigs in Berlin by artists similar to {artist}.</i></li></ul></br>'
+    else:
+        response = '<h1>No Gigs Found In Berlin!</h1>'
+
+    return response
+
+
+@server.route('/artists')
+def artists():
+    response = createNewGigList()
     return jsonify(response)
 
 
